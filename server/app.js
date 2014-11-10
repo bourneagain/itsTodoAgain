31839f9eb21a48339f4f432af3ae817b456a3768
Shyam Rajendran <samateapple@gmail.com>
2014-11-10 12:38:46 -0600

    glyphicon added

--- server/app.js
+++ server/app.js
@@ -4,11 +4,11 @@
         var express       = require('express');
         var path          = require('path');
         var logger        = require('morgan');
-//        var cookieParser  = require('cookie-parser');
-//        var bodyParser    = require('body-parser');
-//        var request       = require('request');
-//        var cookieParser  = require('cookie-parser');
-//        var bodyParser    = require('body-parser');
+        var cookieParser  = require('cookie-parser');
+        var bodyParser    = require('body-parser');
+        var request       = require('request');
+        var cookieParser  = require('cookie-parser');
+        var bodyParser    = require('body-parser');
 
         var routes = require('./routes/index');
 
